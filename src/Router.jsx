a82d0d6389165cba22576b1d5b92cf6b76337e26
RaiczuK <86255165+RaiczuK@users.noreply.github.com>
2023-03-11 00:14:32 +0100

    css fix

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Axios } from 'axios';
-import Nav from './components/Nav.jsx';
+import Nav from './components/Nav';
 import Home from './pages/Home';
 
 const App = () => {
