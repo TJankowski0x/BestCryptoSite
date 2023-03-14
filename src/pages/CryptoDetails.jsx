4e65963661298872abe4840e80a97f2ed84041b9
TotallyNotVulpus <mrbiznesmen@wp.pl>
2023-03-14 23:02:07 +0100

    2nd page styling

--- src/pages/CryptoDetails.jsx
+++ src/pages/CryptoDetails.jsx
@@ -20,7 +20,7 @@ const CryptoDetails = () => {
         isError
     } = useQuery('cryptoDetails', () => {
         return Axios.get(
-            `https://api.coingecko.com/api/v3/coins/${cryptoID}?community_data=false&sparkline=true`
+            `https://api.coingecko.com/api/v3/coins/${cryptoID}?community_data=false&sparkline=true&price_change_percentage=1h%2C24h%2C7d`
         ).then((res) => res.data);
     });
 
@@ -32,45 +32,94 @@ const CryptoDetails = () => {
         return <div>Error...</div>;
     }
 
+    const styles = {
+        colorgreen: {
+            backgroundColor: cryptoInfo.market_data.price_change_percentage_1h_in_currency.usd>0?
+             "green":"red"
+        }
+    }
+
     return (
         <div className="details-container">
             <div className="head-info">
+                <img src= {cryptoInfo.image.large} alt="coinlogo"/>
                 <div className="crypto-name">
                     <h1>{cryptoInfo.name}</h1>
                 </div>
-                <div className="head-price">
-                    <h2>{`$${cryptoInfo.market_data.current_price.usd.toLocaleString(undefined, {
-                        minimumFractionDigits: 2,
-                        maximumFractionDigits: 2
-                    })}`}</h2>
+                <div className="head-box">
+                    <div className="head-price">
+                        <h2>{`$${cryptoInfo.market_data.current_price.usd.toLocaleString(undefined, {
+                            minimumFractionDigits: 2,
+                            maximumFractionDigits: 2
+                        })}`}</h2>
+                    </div>
                 </div>
             </div>
             <div className="rest-data">
                 <div className="left-info">
                     <div className="basic-data">
+                        <p className='header'>Basic Data</p>
                         <BoxInfo
-                            text="Price"
-                            whiteInfo={`$${cryptoInfo.market_data.current_price.usd.toLocaleString(
-                                undefined,
-                                {
-                                    minimumFractionDigits: 2,
-                                    maximumFractionDigits: 2
-                                }
-                            )}`}
+                            text="Market Cap Rank"
+                            whiteInfo={`#${cryptoInfo.market_cap_rank}`}
                         />
                         <BoxInfo
-                            text="Market Cup"
+                            text="Market Cap"
                             whiteInfo={`$${cryptoInfo.market_data.market_cap.usd.toLocaleString()}`}
                         />
                         <BoxInfo
-                            text="Total volume"
+                            text="Trading Volume 24h"
                             whiteInfo={`$${cryptoInfo.market_data.total_volume.usd.toLocaleString()}`}
                         />
-                        <BoxInfo text="Trading Volume(24h)/cos tu sie wymysli" />
+                        <BoxInfo text="All-time high" whiteInfo={`$${cryptoInfo.market_data.ath.usd}`}/>
+                    </div>
+                    <div className="bot-box">
+                        <div className="price-data">
+                            <p className="header">Price Data</p>
+                            <BoxInfo style={styles.colorgreen}
+                            text="Last 1h"
+                            whiteInfo={cryptoInfo.market_data.price_change_percentage_1h_in_currency.usd>0?
+                                "+" + cryptoInfo.market_data.price_change_percentage_1h_in_currency.usd.toFixed(2):
+                                cryptoInfo.market_data.price_change_percentage_1h_in_currency.usd.toFixed(2) + "%"}
+                            />
+                            <BoxInfo style={{backgroundColor:
+                            cryptoInfo.market_data.price_change_percentage_24h_in_currency.usd>0?
+                            'green':
+                            'red'}}
+                            text="Last 1h"
+                            whiteInfo={cryptoInfo.market_data.price_change_percentage_24h_in_currency.usd>0?
+                                "+" + cryptoInfo.market_data.price_change_percentage_24h_in_currency.usd.toFixed(2) + "%":
+                                cryptoInfo.market_data.price_change_percentage_24h_in_currency.usd.toFixed(2) + "%"}
+                            />
+                            <BoxInfo style={`${
+                            cryptoInfo.market_data.price_change_percentage_7d_in_currency.usd>0?
+                            {backgroundColor: 'green'}:
+                            {backgroundColor: 'red'}}`}
+                            text="Last 7d"
+                            whiteInfo={cryptoInfo.market_data.price_change_percentage_7d_in_currency.usd>0?
+                                "+" + cryptoInfo.market_data.price_change_percentage_7d_in_currency.usd.toFixed(2) + "%":
+                                cryptoInfo.market_data.price_change_percentage_7d_in_currency.usd.toFixed(2) + "%"}
+                            />
+                        </div>
+                        <div className="supply-data">
+                        <p className="header">Supply Data</p>
+                        <BoxInfo
+                            text="Max Supply"
+                            whiteInfo={cryptoInfo.market_data.max_supply}
+                        />
+                        <BoxInfo
+                            text="Circulating Supply"
+                            whiteInfo={cryptoInfo.market_data.circulating_supply}
+                        />
+                        <BoxInfo
+                            text="Total Supply"
+                            whiteInfo={cryptoInfo.market_data.total_supply}
+                        />
+                        </div>
                     </div>
                 </div>
                 <div className="right-info">
-                    <div className="exchanges"></div>
+                    <div className="description" dangerouslySetInnerHTML= {{__html: cryptoInfo.description.en}}></div>
                 </div>
             </div>
         </div>
